d406069235d6b5b2a3bec9d6d8125c249d7735a5
Maedbh King <maedbhking@gmail.com>
2023-08-12 16:04:20 -0400

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='survey',
+    packages=find_packages(),
+    version='0.1.0',
+    description='POWER survey on parental leave and childcare at MIT',
+    author='Maedbh King',
+    license='MIT',
+)
